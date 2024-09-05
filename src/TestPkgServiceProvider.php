6943c9772da38e2d727b82cf66ae21fae085ab6b
VDHSoft-com <139960941+VDHSoft-com@users.noreply.github.com>
2024-09-05 16:24:02 +0200

    update

--- src/TestPkgServiceProvider.php
+++ src/TestPkgServiceProvider.php
@@ -7,14 +7,6 @@ use Illuminate\Support\ServiceProvider;
 
 class TestPkgServiceProvider extends ServiceProvider
 {
-    public function register() // register the services
-    {
-		// Enregistre une instance de 'Example' sous le nom 'testpkg' dans le container
-        $this->app->singleton('testpkg', function($app) {
-            return new Example();
-        });
-    }
-
     /**
     * Bootstrap any application services.
     *
@@ -24,12 +16,20 @@ class TestPkgServiceProvider extends ServiceProvider
     {
 		// Boot methods like routes or views can be placed here
         // Charger les vues du package
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'testpackage');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'testpkg');
 
         // Si vous voulez permettre la publication des vues dans le projet principal
         $this->publishes([
-            __DIR__.'/resources/views' => resource_path('views/vendor/testpackage'),
+            __DIR__.'/resources/views' => resource_path('views/vendor/testpkg'),
         ], 'views');
 
+    }
+
+    public function register() // register the services
+    {
+		// Enregistre une instance de 'Example' sous le nom 'testpkg' dans le container
+        $this->app->singleton('testpkg', function($app) {
+            return new Example();
+        });
     }
 }
--- src/resources/views/example.blade.php
+++ src/resources/views/example.blade.php
@@ -1,2 +1,2 @@
 <!-- resources/views/example.blade.php -->
-<h1>Welcome to Laravel Test Package</h1>
+<h1>Welcome to VDHSoft Laravel Test Package</h1>
